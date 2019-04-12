8aac0e5f1c9a40835dab8ec3c91c5db056b0675d
unknown <riyad298@gmail.com>
2019-04-12 11:46:28 +0600

    commit by alias
    this is used for short cut puspose

--- model/db.js
+++ model/db.js
@@ -1,33 +1,11 @@
-//var mysql = require('mysql');
+
 var oracledb = require('oracledb');
-
-
-/*var config = {
-	host: '127.0.0.1',
-	user: 'root',
-	password: '',
-	database: 'node_project',
-	multipleStatements: true
-};*/
 
 var config = {
 	user          : 'scott',
     password      : 'tiger' ,
     connectString : 'localhost:1521/xe'
 }
-
-
-
-/*function getConnection(){
-	var connection = mysql.createConnection(config);
-	connection.connect(function(err){
-		if(err){
-			console.log(err.stack);
-		}
-		console.log('connection id is: '+ connection.threadId);
-	});
-	return connection;
-}*/
 
 function doRelease(connection) {
   connection.close(
@@ -39,48 +17,8 @@ function doRelease(connection) {
 }
 
 
-
-function getConnection(){
-	oracledb.getConnection( 
-
-		config
-	 ,  
-		function(err , connection){
-			if(err){
-	      		console.error(err.message);
-				return;
-			}
-			
-
-			connection.execute(sql , function(err, result){
-			if(err){
-          		console.error(err.message);
-				callback([]);
-			}else{
-				console.log(result.rows); 
-				callback(result.rows);
-			}
-		});
-
-			connection.close(function(err){
-			
-			if (err) {
-        		console.error(err.message);
-     				 }
-			});
-
-
-		}		
-		);
-	
-}
-
-//var pool  = mysql.createPool(config);
-
-
-
 module.exports ={
-execute: function(sql , callback){
+getResult: function(sql , callback){
 
 oracledb.getConnection(
   config,
@@ -106,20 +44,38 @@ oracledb.getConnection(
       });
   });
 
-// Note: connections should always be released when not needed
+},
 
 
+execute: function(sql , callback){
 
-
-
-
-
-
-
+oracledb.getConnection(
+  config,
+  function(err, connection) {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    connection.execute(
+    
+      sql,
+   
+      function(err, status) {
+        if (err) {
+          console.error(err.message);
+          doRelease(connection);
+          callback(false);
+        }
+        
+        console.log(status);    
+        doRelease(connection);
+        callback(status);
+      });
+  });
 
 }
 
- }
+}
 
 
 
